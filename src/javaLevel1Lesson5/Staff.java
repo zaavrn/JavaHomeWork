17467f0548c4a705f2567a80a129ba7d7296b25b
zaavrn <zaa_vrn@mail.ru>
2020-02-05 20:16:02 +0300

    Домашние задание к 5 уроку. Попробовал сделать с вводом возраста для отбора. Перезолил в эту же ветку 2 урок, который ранее был не правильно залит на Git. Извините за задержку.

--- src/javaLevel1Lesson5/Staff.java
+++ src/javaLevel1Lesson5/Staff.java
@@ -1,9 +1,11 @@
 package javaLevel1Lesson5;
 
-public class User {
+import java.util.Objects;
+
+public class Staff {
 
     private String firstName;
-    private String midelName;
+    private String middleName;
     private String surname;
 
     private String position;
@@ -12,9 +14,9 @@ public class User {
     private int salary;
     private int age;
 
-    public User(String firstName, String surname, String position, String email, String phoneNumber,int salary, int age){
+    public Staff(String firstName, String middleName, String surname, String position, String email, String phoneNumber, int salary, int age){
         this.firstName = firstName;
-        this.midelName = middeleName;
+        this.middleName = middleName;
         this.surname = surname;
         this.position = position;
         this.email = email;
@@ -29,6 +31,14 @@ public class User {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
     }
 
     public String getSurname() {
@@ -65,10 +75,14 @@ public class User {
 
     public int getSalary() {
         return salary;
-    }
+          }
 
     public void setSalary(int salary) {
-        this.salary = salary;
+        if (salary>0){
+            this.salary = salary;
+        }else{
+            System.out.println("Зарплата должна быть больше 0");
+        }
     }
 
     public int getAge() {
@@ -79,22 +93,14 @@ public class User {
         this.age = age;
     }
 
-    public void changePosition (String position){
-       this.position=position;
-        System.out.println(firstName+position);
-    }
-
-
-
-
 
     public void info(){
 
-        System.out.println("Имя: " + firstName + "; Фамилия: "+ surname + "; Должность: " + position + "; E-mail: " + email + "; Номер телефона: +" + phoneNumber + "; Заработная плата: " + salary + "; Возраст: " + age);
+        System.out.println("Имя: " + firstName + "; Отчество: " + middleName + "; Фамилия: "+ surname + "; Должность: " + position + "; E-mail: " + email + "; Номер телефона: +" + phoneNumber + "; Заработная плата: " + salary + "; Возраст: " + age);
     }
 
 
-    @Override
+    /*@Override
     public String toString() {
         return "User{" +
                 "firstName='" + firstName + '\'' +
@@ -105,32 +111,25 @@ public class User {
                 ", salary=" + salary +
                 ", age=" + age +
                 '}';
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Staff staff = (Staff) o;
+        return salary == staff.salary &&
+                age == staff.age &&
+                firstName.equals(staff.firstName) &&
+                middleName.equals(staff.middleName) &&
+                surname.equals(staff.surname) &&
+                position.equals(staff.position) &&
+                email.equals(staff.email) &&
+                phoneNumber.equals(staff.phoneNumber);
     }
 
-    public static void main(String[] args) {
-        User staff = new User( "Иван","Иванов Иван Федорович", "Системный администратор", "it@mail.ru", "7(919)235-69-70", 100000, 30);
-       staff.info();
-       staff.changePosition("boss");
-       staff.info();
-
-
-
-
-        /*User [] staffArray = new User[5];
-
-        staffArray [0] = new User( "Иванов Иван Федорович", "Системный администратор", "it@mail.ru", "7(919)235-69-70", 100000, 30);
-        staffArray [1] = new User( "Петров Иван Иванович", "Бухгалтер", "buh@mail.ru", "7(919)235-69-71", 100000, 45);
-        staffArray [2] = new User( "Сидоров Иван Иванович", "Менеджер", "sii@mail.ru", "7(919)235-69-72", 100000, 41);
-        staffArray [3] = new User( "Зайцева Ольга Петровна", "офис менеджер", "info@mail.ru", "7(919)235-69-73", 100000, 20);
-        staffArray [4] = new User( "Путин Владимир Владимирович", "BOSS", "boss@mail.ru", "1(111)111-11-11", 1000000, 65);
-
-
-        //User staff = new User ("Иванов Иван Федорович", "Программист", "info@mail.ru", "7(919)235-69-70", 100000, 30);
-
-        staff.info();*/
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, middleName, surname, position, email, phoneNumber, salary, age);
     }
-
-
 }
--- src/javaLevel1Lesson5/StaffSearch.java
+++ src/javaLevel1Lesson5/StaffSearch.java
@@ -1,4 +1,42 @@
 package javaLevel1Lesson5;
 
+import java.util.Scanner;
+
 public class StaffSearch {
+
+    private  static final int COUNT = 5;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        Staff [] staffArray = new Staff[COUNT];
+
+        staffArray [0] = new Staff( " Иван ","Федорович" ,"Иванов", "Системный администратор", "it@mail.ru", "7(919)235-69-70", 100000, 30);
+        //staffArray [1] = new Staff( " Иван ","Федорович" ,"Иванов", "Системный администратор", "it@mail.ru", "7(919)235-69-70", 100000, 30);
+        staffArray [1] = new Staff( "Иван", "Иванович", "Петров","Бухгалтер", "buh@mail.ru", "7(919)235-69-71", 60000, 45);
+        staffArray [2] = new Staff(  "Иван", "Иванович","Сидоров", "Менеджер", "sii@mail.ru", "7(919)235-69-72", 80000, 41);
+        staffArray [3] = new Staff(  "Ольга", "Петровна","Зайцева", "офис менеджер", "info@mail.ru", "7(919)235-69-73", 40000, 20);
+        staffArray [4] = new Staff(  "Владимир", "Владимирович","Путин", "BOSS", "boss@mail.ru", "1(111)111-11-11", 10000000, 65);
+//                staffArray[0].setSalary(-200000);
+//                System.out.println(staffArray[0].equals(staffArray[1]))
+//                System.out.println(staffArray[1]);
+//                System.out.println(staffArray[0]);
+        searchAge(staffArray);
+
+    }
+
+    private static void searchAge(Staff[] staffArray) {
+        System.out.print("Веедите возраст сотрудника для отбора свыше лет:");
+        int ageinput = scanner.nextInt();
+        for (Staff x: staffArray ){
+            if (x.getAge()>ageinput){
+                x.info();
+            }
+        }
+    }
+
+
 }
+
+
